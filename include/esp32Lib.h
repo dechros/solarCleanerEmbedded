60ac2a31c78e53ca5439baa2cfe20a61ed1374b7
MixhizoR <oguzselmancetin@hotmail.com>
2023-05-17 16:33:21 +0300

    added sendString

---
+++ include/esp32Lib.h
@@ -0,0 +1,8 @@
+#ifndef ESP32_LIB_H
+#define ESP32_LIB_H
+
+void sendATCommand(char const* command);
+void startESP32();
+void sendString(char const* sentence);
+
+#endif  // ESP32_LIB_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,18 @@
 #include <Arduino.h>
+#include "esp32Lib.h"
 // #include "CAN.h"
 
-void sendATCommand(const char* command) {
-  Serial1.println(command);
-  delay(1000); // Wait for the response
-  while (Serial1.available()) {
-    Serial.write(Serial1.read());
-  }
-}
+
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial1.begin(115200);
-
-  delay(2000); // Wait for the module to initialize
-
-  sendATCommand("AT+CWMODE=1"); // Set ESP32 to station mode
-  delay(2000); // Wait for the mode change
-
-  sendATCommand("AT+PASSCHANNEL=1"); // Reset the module
-  delay(2000); // Wait for the reset
-
-  sendATCommand("AT+CIPETH_DEF=\"192.168.1.254\",\"192.168.1.1\",\"255.255.255.0\""); // Reset the module
-  delay(2000); // Wait for the reset
-
-  // sendATCommand("AT+CWDHCP_DEF=3,0"); //! Giving error to this code (This code sets up DHCP)
-  // delay(2000); // Wait for the reset
-
-  sendATCommand("AT+CIFSR");
-  delay(4000);
-
-  sendATCommand("AT+CIPETH_DEF?");
-  delay(2000);
-
-  sendATCommand("AT+CIPSTART=\"TCPC\",\"192.168.1.50\",1234");
-  delay(2000);
-
-  sendATCommand("AT+CIPSEND=4");
-  sendATCommand("cu12");
-  delay(2000);
+  startESP32();
 }
 
 void loop()
 {
     delay(1000);
-    sendATCommand("AT+CIPSEND=4");
-    sendATCommand("qwer");
-    //String message = "APSJDUIAFJH"; // Değişken, seri porttan gelen mesajı tutacak
-    //String sendCommand = "AT+CIPSEND=" + String(message.length() + 2) + "\r\n" + message + "\r\n"; // CIPSEND komutunu oluştur
-    //Serial1.println(sendCommand); // CIPSEND komutunu gönder
-    //Serial.println("sendCommand"); // CIPSEND komutunu gönder
+    sendString("naber mudur");
 }
 
 
